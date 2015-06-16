a1e31fdad07e7bfc0196e7f62046b109a340dcea
Takahiro Kasanami <dev.kacha@gmail.com>
2015-06-16 23:41:45 +0900

    シングルトン化

--- Assets/Ksnm/AdMobManager/AdMobManager.cs
+++ Assets/Ksnm/AdMobManager/AdMobManager.cs
@@ -1,24 +1,25 @@
 ﻿/*
- Copyright (c) 2015 Takahiro Kasanami
- 
- This software is provided 'as-is', without any express or implied
- warranty. In no event will the authors be held liable for any damages
- arising from the use of this software.
- 
- Permission is granted to anyone to use this software for any purpose,
- including commercial applications, and to alter it and redistribute it
- freely, subject to the following restrictions:
- 
-    1. The origin of this software must not be misrepresented; you must not
-    claim that you wrote the original software. If you use this software
-    in a product, an acknowledgment in the product documentation would be
-    appreciated but is not required.
- 
-    2. Altered source versions must be plainly marked as such, and must not be
-    misrepresented as being the original software.
- 
-    3. This notice may not be removed or altered from any source
-    distribution.
+The zlib License
+
+Copyright (c) 2015 Takahiro Kasanami
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source distribution.
 */
 using UnityEngine;
 using GoogleMobileAds.Api;
@@ -28,7 +29,7 @@ namespace Ksnm
     /// <summary>
     /// AdMob広告の管理
     /// </summary>
-    public class AdMobManager : MonoBehaviour
+    public class AdMobManager : SingletonMonoBehaviour<AdMobManager>
     {
         [SerializeField]
         private string androidBannerUnitId;
@@ -66,7 +67,10 @@ namespace Ksnm
 
         private InterstitialAd interstitial = null;
 
-        void Awake()
+        /// <summary>
+        /// Awakeの代わり
+        /// </summary>
+        protected override void OnAwake()
         {
             // 起動時にインタースティシャル広告をロード
             LoadInterstitial();
